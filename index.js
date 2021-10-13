9005bd611b7914501775a94cd747cd9d9181ea68
Marat Oshmiansky <maratical@gmail.com>
2021-10-13 12:59:10 -0700

    fix chart to show Vue repo data

--- index.js
+++ index.js
@@ -1,39 +1,44 @@
-/* global Chart */
+/* global Chart, axios */
 
-var ctx = document.getElementById("myChart").getContext("2d");
-var myChart = new Chart(ctx, {
-  type: "bar",
-  data: {
-    labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
-    datasets: [
-      {
-        label: "# of Votes",
-        data: [12, 19, 3, 5, 2, 3],
-        backgroundColor: [
-          "rgba(255, 99, 132, 0.2)",
-          "rgba(54, 162, 235, 0.2)",
-          "rgba(255, 206, 86, 0.2)",
-          "rgba(75, 192, 192, 0.2)",
-          "rgba(153, 102, 255, 0.2)",
-          "rgba(255, 159, 64, 0.2)",
+axios
+  .get("https://api.github.com/repos/vuejs/vue")
+  .then(function (response) {
+    // handle success
+    console.log(response.data);
+    let stars = response.data.stargazers_count;
+    let watchers = response.data.watchers_count;
+    let forks = response.data.forks_count;
+    console.log("Stars: ", stars);
+    console.log("Watchers: ", watchers);
+    console.log("Forks: ", forks);
+    var ctx = document.getElementById("myChart").getContext("2d");
+    var myChart = new Chart(ctx, {
+      type: "bar",
+      data: {
+        labels: ["Stars", "Watchers", "Forks"],
+        datasets: [
+          {
+            label: "# of Stars, Watchers and Forks",
+            data: [stars, watchers, forks],
+            backgroundColor: ["rgba(255, 99, 132, 0.2)", "rgba(54, 162, 235, 0.2)", "rgba(255, 206, 86, 0.2)"],
+            borderColor: ["rgba(255, 99, 132, 1)", "rgba(54, 162, 235, 1)", "rgba(255, 206, 86, 1)"],
+            borderWidth: 1,
+          },
         ],
-        borderColor: [
-          "rgba(255, 99, 132, 1)",
-          "rgba(54, 162, 235, 1)",
-          "rgba(255, 206, 86, 1)",
-          "rgba(75, 192, 192, 1)",
-          "rgba(153, 102, 255, 1)",
-          "rgba(255, 159, 64, 1)",
-        ],
-        borderWidth: 1,
       },
-    ],
-  },
-  options: {
-    scales: {
-      y: {
-        beginAtZero: true,
+      options: {
+        scales: {
+          y: {
+            beginAtZero: true,
+          },
+        },
       },
-    },
-  },
-});
+    });
+  })
+  .catch(function (error) {
+    // handle error
+    console.log(error);
+  })
+  .then(function () {
+    // always executed
+  });
